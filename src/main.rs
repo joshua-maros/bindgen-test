9903949a1ca1f75f33a17bfc3867705cd5b8dae3
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2021-02-02 13:30:38 -0800

    test 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    let path = PathBuf::from("C:\\msys64\\mingw64\\bin\\libclang.dll");
+    let lib = libloading::Library::new(&path);
+    if let Err(e) = lib {
+        println!("{:?}", e);
+    } else {
+        panic!("It worked fine?");
+    }
 }
